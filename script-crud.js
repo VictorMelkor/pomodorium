c444e53e5125592c10bd38dae50e07227459d80a
Victor Rodrigues <vsr1988@hotmail.com>
2025-07-17 17:57:57 -0300

    dinamismo js e localstorage

---
+++ script-crud.js
@@ -0,0 +1,102 @@
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textarea = document.querySelector('.app__form-textarea');
+const ulTarefas = document.querySelector('.app__section-task-list');
+
+const paragrafoDescricaoDaTarefa = document.querySelector('.app__section-active-task-description')
+
+const tasks = JSON.parse(localStorage.getItem('tarefas')) || [];
+let tarefaSelecionada = null
+
+function atualizarTarefas() {
+    localStorage.setItem('tarefas', JSON.stringify(tasks))
+}
+
+function criarElementoTarefa(tarefa) {
+    const li = document.createElement('li')
+    li.classList.add('app__section-task-list-item')
+
+    const svg = document.createElement('svg')
+    svg.innerHTML = `
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+                fill="#01080E"></path>
+        </svg>
+    `
+    const paragrafo = document.createElement('p')
+    paragrafo.textContent = tarefa.descricao
+    paragrafo.classList.add('app__section-task-list-item-description')
+
+    const botao = document.createElement('button')
+    const imgBotao = document.createElement('img')
+    botao.classList.add('app_button-edit')
+
+    botao.onclick = () => {
+        //debugger
+        const novaDescricao = prompt("Qual é o novo nome da tarefa?")
+        //console.log('Nova Descrição da Tarefa: ', novaDescricao)
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao
+            tarefa.descricao = novaDescricao
+            atualizarTarefas()
+        }
+    }
+
+    imgBotao.setAttribute('src', '/imagens/edit.png')
+
+    botao.append(imgBotao)
+
+    li.append(svg)
+    li.append(paragrafo)
+    li.append(botao)
+
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active')
+            })
+
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoDaTarefa.textContent = ''
+            tarefaSelecionada = null
+            return
+        }
+        tarefaSelecionada = tarefa
+        paragrafoDescricaoDaTarefa.textContent = tarefa.descricao
+        
+        li.classList.add('app__section-task-list-item-active')
+        
+    }
+
+    return li
+}
+
+btnAdicionarTarefa.addEventListener("click", () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
+btnCancelar.addEventListener('click', () => {
+    textarea.value = ''
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textarea.value
+    }
+    tasks.push(tarefa);
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+    atualizarTarefas()
+    textarea.value = ''
+    formAdicionarTarefa.classList.add('hidden')
+})
+
+tasks.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+});
